04b74d3fa9ce236153e850f27890de76f55abf19
Gulnur Uzun <157987210+gulnuruzun0@users.noreply.github.com>
2024-06-20 19:30:02 +0300

    Update TFbindingdata_operations.py

--- TFbindingdata_operations.py
+++ TFbindingdata_operations.py
@@ -1,32 +1,21 @@
 '''
-BENG215/BSB501/MBG624 – Biocomputing – Python HW3 – Jan 12th, 23:30, 2023
-
-Group 26
-
-Alper Tanrıkulu
-Gülnur Uzun
-
+TFbinding Data Operations and Visualization
 '''
-
-
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import leastsq
 
-# Question 1
 # Read the data by selecting appropriate columns based on column names in the data file
 myData = pd.read_csv("TFbinding-HW3-F23.txt", delimiter="\t")
 
-# Question 2
 # Calculate the mean and standard deviation of the 'Expression Level' column
 expMean = np.mean(myData["Expression Level"])
 expStd = np.std(myData["Expression Level"])
 print(f"Mean of Expression Level: {expMean}")
 print(f"Standard Deviation of Expression Level: {expStd}")
 
-# Question 3
 # Calculate the total binding score, and calculate its mean and standard deviation
 myData["SumOfBinding"] = myData.iloc[:, 1:7].sum(axis=1)
 sumMean = np.mean(myData["SumOfBinding"])
@@ -34,7 +23,6 @@ sumStd = np.std(myData["SumOfBinding"])
 print(f"Mean of Total Binding Scores: {sumMean}")
 print(f"Standard Deviation of Total Binding Scores: {sumStd}")
 
-# Question 4
 # Plot two histograms as subplots: one for expression levels and one for total binding scores
 plt.figure(figsize=[12, 5]) # Arranged the size of the plots
 
@@ -61,7 +49,6 @@ plt.title("Histogram of Binding Scores")
 plt.legend()
 plt.show()
 
-# Question 5
 # Set input data and initial guesses for linear regression model
 xi = myData["SumOfBinding"]
 yi_exp = myData["Expression Level"]
@@ -79,14 +66,12 @@ coef, _ = leastsq(residuals, p0, args=(yi_exp, xi))
 a, b = coef[0], coef[1]
 print("Fitted Equation: Expression Level =", round(a, 3), "+", round(b, 3), "* Total Binding Score")
 
-# Question 6
 # Predict gene expression level using the linear regression equation obtained
 yi_est = a + b * xi
 error = yi_exp - yi_est  # Error = True_expression_level – Predicted_expression_level for a gene (calculate error)
 print("Predicted Expression Levels:", yi_est)
 print("Prediction Errors:", error)
 
-# Question 7
 # Visualize the results
 plt.figure(figsize=(10, 6))
 plt.scatter(xi, yi_exp, marker='o', label='True Expression Level') #True expression values (circles)
